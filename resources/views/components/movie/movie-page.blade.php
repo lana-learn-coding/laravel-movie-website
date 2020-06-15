d6e04514272a9947888dd7ae9cec0c8b107a00bb
lana <sitdownrigntnow.dev@gmail.com>
2020-06-15 22:41:54 +0700

    show empty message

--- resources/views/components/movie/movie-page.blade.php
+++ resources/views/components/movie/movie-page.blade.php
@@ -1,11 +1,23 @@
 <div class="row">
-    @foreach ($movies as $movie)
-        <div class="col-6 col-md-4 col-xl-3 my-4">
-            <div class="hvr-grow d-flex">
-                @include('components.movie.movie-card', ['movie' => $movie])
+    @if($movies->isEmpty())
+        <div class="col w-100 text-center pt-2">
+            <div class="card">
+                <div class="card-body my-lg-4">
+                    <h5 class="text-muted mb-0">
+                        No Data found :(
+                    </h5>
+                </div>
             </div>
         </div>
-    @endforeach
+    @else
+        @foreach ($movies as $movie)
+            <div class="col-6 col-md-4 col-xl-3 my-4">
+                <div class="hvr-grow d-flex">
+                    @include('components.movie.movie-card', ['movie' => $movie])
+                </div>
+            </div>
+        @endforeach
+    @endif
 </div>
 
 <div class="d-flex justify-content-center mt-4 w-100">
