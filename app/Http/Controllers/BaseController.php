8265325219993d3cc1f3507f0e1e2c7e201c36a2
lana <sitdownrigntnow.dev@gmail.com>
2020-06-17 22:58:06 +0700

    add migration for nation and language, remove unnecessary pivot

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Movie\MovieCategory;
 use App\Models\Movie\MovieGenre;
+use App\Models\Movie\MovieLanguage;
+use App\Models\Movie\MovieNation;
 use View;
 
 class BaseController extends Controller
@@ -12,5 +14,7 @@ class BaseController extends Controller
     {
         View::share('categories', MovieCategory::all());
         View::share('genres', MovieGenre::all());
+        View::share('languages', MovieLanguage::all());
+        View::share('nations', MovieNation::all());
     }
 }
