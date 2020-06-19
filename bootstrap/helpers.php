7e128c95db3c01979ba4b3c7422663e28388aecc
lana <sitdownrigntnow.dev@gmail.com>
2020-06-19 22:44:19 +0700

    add helpers for easily route with query parameter

---
+++ bootstrap/helpers.php
@@ -0,0 +1,18 @@
+<?php
+function route_with_query($name, $queryParameters = [], $parameters = [], $absolute = true)
+{
+    $route = route($name, $parameters, $absolute);
+    if (!empty($queryParameters)) {
+        $route = $route . '?' . http_build_query($queryParameters);
+    }
+    return $route;
+}
+
+function url_with_query($path = null, $queryParameters = [], $parameters = [], $secure = null)
+{
+    $url = url($path, $parameters, $secure);
+    if (!empty($queryParameters)) {
+        $url = $url . '?' . http_build_query($queryParameters);
+    }
+    return $url;
+}
