c9e6bdf1b83d78b881256d2de4219530a7c31f08
Lana <sitdownrightnow2552@gmail.com>
2020-06-27 14:49:28 +0700

    menu seeding

---
+++ database/seeds/AdminMenuSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Encore\Admin\Auth\Database\Menu;
+use Illuminate\Database\Seeder;
+
+
+class AdminMenuSeeder extends Seeder
+{
+    public function run()
+    {
+        $parentId = Menu::insertGetId(['title' => 'Movie', 'icon' => 'fa-bars']);
+        $menus = [
+            ['parent_id' => $parentId, 'title' => 'Movie', 'icon' => 'fa-bars', 'uri' => 'movies'],
+            ['parent_id' => $parentId, 'title' => 'Category', 'icon' => 'fa-bars', 'uri' => 'movies/categories'],
+            ['parent_id' => $parentId, 'title' => 'Genre', 'icon' => 'fa-bars', 'uri' => 'movies/genres'],
+            ['parent_id' => $parentId, 'title' => 'Tag', 'icon' => 'fa-bars', 'uri' => 'movies/tags'],
+            ['parent_id' => $parentId, 'title' => 'Language', 'icon' => 'fa-bars', 'uri' => 'movies/languages'],
+            ['parent_id' => $parentId, 'title' => 'Nation', 'icon' => 'fa-bars', 'uri' => 'movies/nations'],
+            ['parent_id' => $parentId, 'title' => 'Cast', 'icon' => 'fa-bars', 'uri' => 'casts']
+
+        ];
+        Menu::insert($menus);
+    }
+}
