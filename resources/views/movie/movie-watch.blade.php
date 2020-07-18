b6f323dead5fbd4113520a9e375e0e44dea31d08
Lana <sitdownrightnow2552@gmail.com>
2020-07-18 23:51:16 +0700

    install videojs player

--- resources/views/movie/movie-watch.blade.php
+++ resources/views/movie/movie-watch.blade.php
@@ -18,15 +18,24 @@
     </div>
     <div class="card mt-4">
         <div class="card-body card-font-size-lg">
-            <div id="player">
-                <video src=""></video>
-            </div>
+            <video
+                id="video"
+                class="video-js vjs-fluid vjs-16-9"
+            >
+                @foreach($episodes  as $ep)
+                    <source src="{{ url('uploads/' . $ep->file) }}" type="video/mp4" label="{{ $ep->quality }}"/>
+                @endforeach
+                <p class="vjs-no-js">
+                    To view this video please enable JavaScript, and consider upgrading to a
+                    web browser that supports HTML5 video
+                </p>
+            </video>
             <div class="mt-5">
                 <h6 class="card-title font-weight-bold">Episode</h6>
                 <div>
                     @foreach($movie->episode_list as $ep)
                         @if($ep === request()->ep)
-                            <a class="btn shadow-sm btn-sm mr-1 btn-info" href="#player">{{ $ep->number }}</a>
+                            <a class="btn shadow-sm btn-sm mr-1 btn-info" href="#video">{{ $ep->number }}</a>
                         @else
                             <a class="btn shadow-sm btn-sm mr-1 btn-primary"
                                href="{{ route('movie.watch.ep', ['id' => $movie->id, 'ep' => $ep->number]) }}">{{ $ep->number }}</a>
@@ -37,3 +46,23 @@
         </div>
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        options = {
+            controls: true,
+            autoplay: false,
+            preload: 'auto',
+            controlBar: {
+                children: [
+                    'playToggle',
+                    'progressControl',
+                    'volumePanel',
+                    'qualitySelector',
+                    'fullscreenToggle',
+                ],
+            },
+        };
+        const player = videojs('video', options);
+    </script>
+@endpush
