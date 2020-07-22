4fc949b9941c4443323809c47ef21b14d0bc1d30
Lana <sitdownrightnow2552@gmail.com>
2020-07-22 19:36:34 +0700

    remove admin route

--- resources/views/components/app/nav-login.blade.php
+++ resources/views/components/app/nav-login.blade.php
@@ -22,7 +22,6 @@
             </a>
 
             <div class="dropdown-menu dropdown-menu-right">
-                <a class="dropdown-item" href="{{ route('admin') }}">{{ __('Admin') }}</a>
                 <a class="dropdown-item" href="{{ route('logout') }}"
                    onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
