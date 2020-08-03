ad940ac1f8d9a2835ba1b6e9b08478a265f495ff
Lana <sitdownrightnow2552@gmail.com>
2020-08-03 22:00:00 +0700

    put nav into card

--- resources/views/components/user/user-nav.blade.php
+++ resources/views/components/user/user-nav.blade.php
@@ -1,26 +1,30 @@
 @if(Auth::check() && request()->id && request()->id === strval(Auth::id()))
     <div class="pl-2 pl-lg-4 d-none d-md-block">
-        <div class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical">
-            <a class="nav-link {{ Route::is('user.detail') ? 'active': '' }}"
-               href="{{ route('user.detail', ['id' => Auth::id()]) }}">
-                Profile
-            </a>
-            <a class="nav-link {{ Route::is('user.update') ? 'active': '' }}"
-               href="{{ route('user.update', ['id' => Auth::id()]) }}">
-                Account
-            </a>
-            <a class="nav-link {{ Route::is('user.favorite') ? 'active': '' }}"
-               href="{{ route('user.favorite', ['id' => Auth::id()]) }}">
-                Favorites
-            </a>
-            <a class="nav-link {{ Route::is('user.rated') ? 'active': '' }}"
-               href="{{ route('user.rated', ['id' => Auth::id()]) }}">
-                Rated
-            </a>
+        <div class="card">
+            <div class="card-body p-3">
+                <div class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical">
+                    <a class="nav-link {{ Route::is('user.detail') ? 'active': '' }}"
+                       href="{{ route('user.detail', ['id' => Auth::id()]) }}">
+                        Profile
+                    </a>
+                    <a class="nav-link {{ Route::is('user.update') ? 'active': '' }}"
+                       href="{{ route('user.update', ['id' => Auth::id()]) }}">
+                        Account
+                    </a>
+                    <a class="nav-link {{ Route::is('user.favorite') ? 'active': '' }}"
+                       href="{{ route('user.favorite', ['id' => Auth::id()]) }}">
+                        Favorites
+                    </a>
+                    <a class="nav-link {{ Route::is('user.rated') ? 'active': '' }}"
+                       href="{{ route('user.rated', ['id' => Auth::id()]) }}">
+                        Rated
+                    </a>
 
-            <a class="nav-link" href="#" data-logout-warning>
-                Logout
-            </a>
+                    <a class="nav-link" href="#" data-logout-warning>
+                        Logout
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 @else
