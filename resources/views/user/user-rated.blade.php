685d8c7d05aaabd749d4fb3850b1a7da3928ae9d
Lana <sitdownrightnow2552@gmail.com>
2020-08-26 01:25:36 +0700

    migrate user display to vuetify

--- resources/views/user/user-rated.blade.php
+++ resources/views/user/user-rated.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.user')
 
 @section('content.body.user')
-    <div class="mt-4">
-        <h4 class="mb-2">Rated Movies</h4>
-        <hr class="border-info mt-2 mb-0">
+    <div class="mt-4 mt-md-5 mt-lg-8 mt-xl-9">
+        <h4 class="text-h6 mb-2">Rated Movies</h4>
+        <v-divider></v-divider>
         @include('components.movie.movie-page', ['movies' => $rateds])
     </div>
 @endsection
