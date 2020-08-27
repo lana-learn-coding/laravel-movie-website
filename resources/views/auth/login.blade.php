b9647537fe873922f698ade2551fed45d32450b0
Lana <sitdownrightnow2552@gmail.com>
2020-08-27 08:09:23 +0700

    add remember me check box

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,12 +37,28 @@
                                 @enderror
                             >
                             </v-text-field>
+                            <v-checkbox
+
+                                label="Remember Me"
+                                v-model="rememberCheckbox"
+                                true-value="on"
+                                dense
+                            >
+                            </v-checkbox>
+                            <input
+                                type="checkbox"
+                                class="d-none"
+                                name="remember"
+                                id="remember"
+                                :checked="rememberCheckbox"
+                                :value="rememberCheckbox ? 'on' : ''"
+                            >
                             <div class="mt-3">
                                 <v-btn type="submit" color="indigo darken-2 mr-4">Login</v-btn>
                                 @if (Route::has('password.request'))
                                     <a class="text--primary text-decoration-none"
                                        href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        Forgot Your Password?
                                     </a>
                                 @endif
                             </div>
@@ -52,4 +68,27 @@
             </v-col>
         </v-row>
     </v-container>
+@endsection
+
+@section('vue')
+    <script src="{{ asset('js/use.js') }}"></script>
+    <script>
+        const app = new Vue({
+            el: '#app',
+            vuetify,
+            setup() {
+                return {
+                    ...useApplicationBase(),
+                    ...useLogin(),
+                };
+            },
+        });
+
+        function useLogin() {
+            const rememberCheckbox = ref({{ old('remember') ? 'true' : 'false' }});
+            return {
+                rememberCheckbox,
+            };
+        }
+    </script>
 @endsection
